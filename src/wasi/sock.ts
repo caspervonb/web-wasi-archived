bceeb398f5b04513dfaac986282cf0a683fb824f
Casper Beyer <caspervonb@pm.me>
2020-03-03 23:17:09 +0800

    Add sock.ts module
    This adds an implementation of the ephemeral_sock module.

---
+++ src/wasi/sock.ts
@@ -0,0 +1,14 @@
+export function recv(fd, ri_data, ri_flags, ro_datalen, ro_flags)
+{
+	return 52; /* ENOSYS */
+}
+
+export function send(fd, si_data, si_flags, so_datalen)
+{
+	return 52; /* ENOSYS */
+}
+
+export function shutdown(fd, how)
+{
+	return 52; /* ENOSYS */
+}
