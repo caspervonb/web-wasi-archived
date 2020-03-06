b9eb41a4596839c260f027b753df5884db7829c3
Casper Beyer <caspervonb@pm.me>
2020-03-07 07:52:35 +0800

    Source command line arguments from self.args
    This sources command line arguments from the self.env property when it
    is defined.

--- src/wasi/args.ts
+++ src/wasi/args.ts
@@ -2,15 +2,38 @@ import { memory } from "env";
 
 export function get(argv, argv_buf)
 {
+	let args = "args" in self ? self.args : [];
+	let data = new DataView(memory.buffer);
+
+	for (let arg of args) {
+		data.setUint32(argv, argv_buf);
+		argv += 4;
+
+		let string = `${arg}\0`;
+		for (let i = 0; i < string.length; i++) {
+			data.setUint32(argv_buf + i, string.charCodeAt(i));
+		}
+
+		argv_buf += string.length;
+	}
+
+	data.setUint32(argv, 0);
+
 	return 0; /* ESUCCESS */
 }
 
 export function sizes_get(argc, argv_buf_size)
 {
+	let args = "args" in self ? self.args : [];
 	let data = new DataView(memory.buffer);
 
-	data.setBigUint64(argc, 0n);
-	data.setBigUint64(argv_buf_size, 0n);
+	let count = args.length;
+	let size = args.reduce(function(accumulator, arg) {
+		return accumulator + (arg as string).length + 1;
+	}, 0);
+
+	data.setUint32(argc, count);
+	data.setUint32(argv_buf_size, size);
 
 	return 0; /* ESUCCESS */
 }
