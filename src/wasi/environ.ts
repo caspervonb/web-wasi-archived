ae02fe19ede113ce9638c864ea70ce9adaf088f4
Casper Beyer <caspervonb@pm.me>
2020-03-07 03:58:24 +0800

    Source environment variables from self.env
    This sources environment variables a set of default values, as-well as
    from the global self.env property when it is defined.

--- src/wasi/environ.ts
+++ src/wasi/environ.ts
@@ -1,16 +1,50 @@
 import { memory } from "env";
 
+const defaults = {
+	"HOME": "/home/user",
+	"PATH": "/",
+	"PWD": "/",
+	"USER": "user",
+};
+
 export function get(environ, environ_buf)
 {
+	let env = Object.assign(defaults, self["env"]);
+	let data = new DataView(memory.buffer);
+
+	let entries = Object.entries(env);
+	for (let [key, value] of entries) {
+        data.setUint32(environ, environ_buf);
+		environ += 4;
+
+		let string = `${key}=${value}\0`;
+		for (let i = 0; i < string.length; i++) {
+			data.setUint32(environ_buf + i, string.charCodeAt(i));
+		}
+
+		environ_buf += string.length;
+    }
+
+	data.setUint32(environ, 0);
+
 	return 0; /* ESUCCESS */
 }
 
 export function sizes_get(environc, environ_buf_size)
 {
+	let env = Object.assign(defaults, self["env"]);
 	let data = new DataView(memory.buffer);
 
-	data.setBigUint64(environc, 0n);
-	data.setBigUint64(environ_buf_size, 0n);
+	let keys = Object.keys(env);
+	let count = keys.length;
+
+	let entries = Object.entries(env);
+	let size = entries.reduce(function(accumulator, [key, value]) {
+		return accumulator + key.length + (value as string).length + 2;
+	}, 0);
+
+	data.setUint32(environc, count);
+	data.setUint32(environ_buf_size, size);
 
 	return 0; /* ESUCCESS */
 }
