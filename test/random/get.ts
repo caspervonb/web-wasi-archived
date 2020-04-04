8cf4f950b5d794c628f3f780ee327454a19797ab
Casper Beyer <caspervonb@pm.me>
2020-04-05 00:40:02 +0800

    Assert that random.get outputs a valid value
    This adds a simple assert to test that random.get outputs a valid array
    of values where a valid array is measured by the sum of those values
    being greater than zero.

--- test/random/get.ts
+++ test/random/get.ts
@@ -4,5 +4,13 @@ export let memory = new WebAssembly.Memory({
 
 import { get as random_get } from "wasi/random";
 
+let data = new DataView(memory.buffer);
 let errno = random_get(0, 256);
 console.assert(errno == 0);
+
+let sum = 0;
+for (let i = 0; i < 256; i++) {
+	sum += data.getUint8(i);
+}
+
+console.assert(sum > 0);
