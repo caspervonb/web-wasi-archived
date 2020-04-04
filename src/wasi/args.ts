8d38927b77ab77e2f2c5f4759ef099c60fa79182
Casper Beyer <caspervonb@pm.me>
2020-04-04 12:15:38 +0800

    Add support for synchronous file entries
    This adds support for file entries (file descriptors) with synchronous
    read and write operations.

--- src/wasi/args.ts
+++ src/wasi/args.ts
@@ -3,18 +3,18 @@ import { memory } from "env";
 export function get(argv, argv_buf)
 {
 	let args = "args" in self ? self.argv : [];
+
+	let byte = new Uint8Array(memory.buffer);
 	let data = new DataView(memory.buffer);
+	let utf8 = new TextDecoder("utf-8");
 
 	for (let arg of args) {
 		data.setUint32(argv, argv_buf);
 		argv += 4;
 
-		let string = `${arg}\0`;
-		for (let i = 0; i < string.length; i++) {
-			data.setUint32(argv_buf + i, string.charCodeAt(i));
-		}
-
-		argv_buf += string.length;
+		let value = utf8.decode(`${arg}\0`);
+		byte.set(value, argv_buf);
+		argv_buf += value.length;
 	}
 
 	data.setUint32(argv, 0);
